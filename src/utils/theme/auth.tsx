d38d7cb02c0a9297fecb245ab212c6b9a6c91ea0
ramya72002 <nsriramya7@gmail.com>
2024-10-27 17:27:06 +0530

    things done

--- src/utils/theme/auth.tsx
+++ src/utils/theme/auth.tsx
@@ -1,19 +1,24 @@
-// utils/auth.tsx
-'use client'
 import { useRouter } from 'next/navigation';
-import { useEffect, ComponentType } from 'react';
+import { useEffect, ComponentType, useState } from 'react';
 
 export const withAuth = <P extends object>(WrappedComponent: ComponentType<P>) => {
   const AuthenticatedComponent: React.FC<P> = (props) => {
     const router = useRouter();
-    const email = localStorage.getItem('email');
+    const [isAuthChecked, setIsAuthChecked] = useState(false);
+    const [email, setEmail] = useState<string | null>(null);
 
     useEffect(() => {
-      if (!email) {
+      const storedEmail = localStorage.getItem('email');
+      setEmail(storedEmail);
+      if (!storedEmail) {
         // Redirect to the higher authentication page if email is not found
         router.push('/auth/login'); // Replace with your actual higher auth page route
       }
-    }, [email, router]);
+      setIsAuthChecked(true); // Mark as checked after accessing localStorage
+    }, [router]);
+
+    // If the authentication check is not yet completed, render nothing
+    if (!isAuthChecked) return null;
 
     // If the email exists, render the wrapped component
     return email ? <WrappedComponent {...props} /> : null; // Render nothing until redirection
